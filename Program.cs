335d3f2485ff700662d16b9fdc8f461343b07e63
Mad <ned_91@mail.ru>
2022-08-07 01:18:35 +0300

    Добавлено решение задачи и проверено её работоспобность

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк длинна которых меньше либо равна 3.
+using System;
+using static System.Console;
+Clear();
+
+
+string[] M;
+WriteLine("Введите массив строк через пробел");
+string unknown = Console.ReadLine();
+M = unknown.Split(' ');
+var result = new string[M .Length];
+var size = 0;
+foreach (var value in M )
+{
+    if (value.Length <= 3)
+    {
+    result[size] = value;
+    size++;
+    }
+}
+WriteLine(string.Join(" ", result, 0, size));
